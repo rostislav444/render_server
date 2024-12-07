d83f97f051cb1af38161f29d56221f525f528076
Rostislav <rostislav444@gmail.com>
2024-12-07 12:12:31 +0200

    minor changes

---
+++ blender/sh/install_python_3_11.sh
@@ -0,0 +1,2 @@
+apt update
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev libxrender1 libxxf86vm1 libxfixes3 libxi6 libxkbcommon0 libsm6
